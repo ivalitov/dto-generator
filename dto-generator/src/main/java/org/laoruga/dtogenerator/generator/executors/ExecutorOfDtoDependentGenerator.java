908e88a8a3e7da06475961ac298705efdac5f63f
Il'dar Valitov <valitov.i.r@gmail.com>
2023-02-27 13:44:40 +0600

    fix

--- dto-generator/src/main/java/org/laoruga/dtogenerator/generator/executors/ExecutorOfDtoDependentGenerator.java
+++ dto-generator/src/main/java/org/laoruga/dtogenerator/generator/executors/ExecutorOfDtoDependentGenerator.java
@@ -43,7 +43,7 @@ public class ExecutorOfDtoDependentGenerator extends ExecutorOfGenerator {
     protected boolean isDtoReadyForFieldGeneration(IGenerator<?> generator) throws DtoGeneratorException {
         IGenerator<?> usersGeneratorInstance = ((CustomGenerator) generator).getUsersGeneratorInstance();
         boolean dtoReady = ((ICustomGeneratorDtoDependent<?, ?>) usersGeneratorInstance).isDtoReady();
-        log.debug("Object {} ready to generate dependent field value", dtoReady ? "is" : "isn't");
+        log.debug("Object " + (dtoReady ? "is" : "isn't") + " ready to generate dependent field value");
         return dtoReady;
     }
 
